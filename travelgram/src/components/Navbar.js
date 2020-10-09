b0d13b910ad8c69be49d5f45cbe4c54c7d4ab6f4
noob69-dot <arjunople@gmail.com>
2020-10-10 00:38:44 +0530

    updations

---
+++ travelgram/src/components/Navbar.js
@@ -0,0 +1,65 @@
+import React,{useState,useEffect} from 'react'
+import {Link} from 'react-router-dom'
+import { Button } from './Button';
+import './Navbar.css'
+
+function Navbar() {
+    const [click, setClick] = useState(false); //states
+    const [button, setButton] = useState(true);
+
+    const handleClick= () =>setClick(!click)    //handlers
+    const closeMobileMenu= () =>setClick(false)
+
+    const showButton =()=>{
+        if(window.innerWidth <= 960){
+            setButton(false)
+        }
+        else{
+            setButton(true)
+        }
+    }
+    useEffect(() => {
+        showButton();
+    }, [])
+    window.addEventListener('resize',showButton );
+
+    return (
+       <>
+        <nav className="navbar">
+            <div className="navbar-container">
+            
+            <Link to="/" className="navbar-logo">Travelgram</Link>
+            <div className="menu-icon" onClick={handleClick}>
+                {/* add menu icon from font awesomr */}
+                <i className={click ? "fas fa-times" : 'fas fa-bars'}></i>
+            </div>
+            <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className="nav-item">
+                <Link to='/' className="nav-links" onClick={closeMobileMenu}>
+                    Home
+                </Link>
+            </li>
+            <li className="nav-item">
+                <Link to='/travelogues' className="nav-links" onClick={closeMobileMenu}>
+                    Travelogues
+                </Link>
+            </li>
+            <li className="nav-item">
+                <Link to='/about-us' className="nav-links" onClick={closeMobileMenu}>
+                About us
+                </Link>
+            </li>
+            <li className="nav-item">
+                <Link to='/sign-up' className="nav-links-mobile" onClick={closeMobileMenu}>
+                   Sign up
+                </Link>
+            </li>
+            </ul>
+            {button && <Button buttonStyle='btn--outline'>Sign Up</Button>}
+            </div>
+        </nav>
+       </>
+    )
+}
+
+export default Navbar
