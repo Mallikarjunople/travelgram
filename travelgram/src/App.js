0c6e8f7fc258a17f6a30021033af88734c503b28
noob69-dot <arjunople@gmail.com>
2020-10-10 23:13:32 +0530

    updated navbar style

--- travelgram/src/App.js
+++ travelgram/src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import './App.css';
+import Home from './components/pages/Home';
 import Navbar from './components/Navbar'
 function App() {
   return (
     <>
      <Router>
        <Navbar/>
-     <Switch>
-        <Route path='/' exact/>
+        <Switch>
+         <Route path='/' exact component={Home} />
       
-      </Switch>          
+        </Switch>          
      
        </Router>
 
